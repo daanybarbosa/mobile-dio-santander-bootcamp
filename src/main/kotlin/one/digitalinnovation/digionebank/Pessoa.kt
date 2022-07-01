7fb9c2aad967a3fc698aec183238fa9d091a1660
Daniele Barbosa da Silva <danielebsilva@MacBook-Pro-de-Daniele.local>
2022-07-01 16:09:06 -0300

    update person class

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -3,6 +3,12 @@ package one.digitalinnovation.digionebank
 class Pessoa {
     var nome: String = "Daniele"
     var cpf: String = "123.123.123-12"
+
+    /* Exemplo de inner class
+    inner class Endereco {
+        var rua: String = "Rua teste"
+    }
+    */
 }
 
 fun main(){
@@ -10,4 +16,6 @@ fun main(){
 
     println(daniele.nome)
     println(daniele.cpf)
+
+    //println(daniele.Endereco().rua) //Exemplo de inner class
 }
