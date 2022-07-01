05c86f80492a49d48dbcc6b4adf35b80723b5fea
Daniele Barbosa da Silva <danielebsilva@MacBook-Pro-de-Daniele.local>
2022-07-01 16:28:15 -0300

    update to apply the private modifier to set function of cpf property

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -4,18 +4,15 @@ class Pessoa {
     var nome: String = "Daniele"
     var cpf: String = "123.123.123-12"
 
-    /* Exemplo de inner class
-    inner class Endereco {
-        var rua: String = "Rua teste"
-    }
-    */
+    //private var cpf: String = "123.123.123-12" //colocando o private direto na propriedade , torna o set e get privados
+    private set //modificador de visibilidade - apenas o set (modificador) será privado.
 }
 
 fun main(){
     val daniele = Pessoa()
+    //daniele.cpf = "1234" //set - setando as informações
 
-    println(daniele.nome)
+    println(daniele) //referencia do objeto em memoria
+    println(daniele.nome) //get - pegando as informações
     println(daniele.cpf)
-
-    //println(daniele.Endereco().rua) //Exemplo de inner class
 }
